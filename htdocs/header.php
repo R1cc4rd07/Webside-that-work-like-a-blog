4b603b43b6dcffd8bacd88274c2e260fcfe585a8
R1cc4rd07 <33084228+R1cc4rd07@users.noreply.github.com>
2017-10-25 15:08:19 +0200

    Add files via upload

--- htdocs/header.php
+++ htdocs/header.php
@@ -6,7 +6,25 @@
 	<link rel="stylesheet" type="text/css" href="css/Header,nav,footer.css">
 	<link rel="stylesheet" type="text/css" href="css/section.css">
 </head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="javascript/login.js"></script>
 <body>
+	<div class="loginbar">
+		<div class="login">
+			<button type="button" class="loginbtn">login</button>
+			<div class="dropdown-content">
+				<form action="?login=1" method="post">
+					E-Mail:<br>
+					<input type="email" size="20" maxlength="250" name="email"><br><br>
+
+					Dein Passwort:<br>
+					<input type="password" size="20"  maxlength="20" name="passwort"><br>
+
+					<input type="submit" value="Abschicken">
+				</form>
+			</div>
+		</div>
+	</div>
 	<header class="HeaderImg"> </header>
 	<nav>
 		<label for="show-menu" class="show-menu"><span>&#9776;</span> Navigation</label>
